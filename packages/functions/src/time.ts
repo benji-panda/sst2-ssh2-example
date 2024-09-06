87be4c865988ead1a18a5279def7f2f64ed90c8e
ben <me@benhunt.io>
2024-09-06 14:50:45 -0500

    reproduced error, no loader is configured for .node files

--- packages/functions/src/time.ts
+++ packages/functions/src/time.ts
@@ -1,6 +1,9 @@
 import { ApiHandler } from "sst/node/api";
+import { Client } from 'ssh2-sftp-client'
 
 export const handler = ApiHandler(async (evt) => {
+
+  console.log(`Client name: ${Client.name}`);
   return {
     statusCode: 200,
     body: evt.requestContext.time,
