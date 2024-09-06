1355348becadaf899cda04e66a3735ed5f8a83b4
ben <me@benhunt.io>
2024-09-06 15:16:44 -0500

    sftp client working using file loader

--- packages/functions/src/time.ts
+++ packages/functions/src/time.ts
@@ -1,9 +1,9 @@
 import { ApiHandler } from "sst/node/api";
-import { Client } from 'ssh2-sftp-client'
+import Client from 'ssh2-sftp-client'
 
 export const handler = ApiHandler(async (evt) => {
 
-  console.log(`Client name: ${Client.name}`);
+  console.log(`Client: ${Client.name}`)
   return {
     statusCode: 200,
     body: evt.requestContext.time,
